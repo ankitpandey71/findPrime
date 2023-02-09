4dcb54b77c595b3ac0b9af356ca420e14a69bec6
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-02-09 10:25:45 +0530

    find Prime Added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = (num) => {
+  if (num % 2 === 0) {
+    return false;
+  } else true;
+
+  console.log(App(5));
+  return <div></div>;
+};
+
+export default App;
