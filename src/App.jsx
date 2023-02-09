1370129c11dc1eae485c7094e3bfa88fdd4dace2
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-02-09 10:27:17 +0530

    added prime function code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-const App = (num) => {
-  if (num % 2 === 0) {
-    return false;
-  } else true;
+const App = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
 
-  console.log(App(5));
-  return <div></div>;
+  const isPrime = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isPrime(input)) {
+      setResult(`${input} is a prime number.`);
+    } else {
+      setResult(`${input} is not a prime number.`);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button type="submit">Check Prime</button>
+      </form>
+      <p>{result}</p>
+    </div>
+  );
 };
 
 export default App;
